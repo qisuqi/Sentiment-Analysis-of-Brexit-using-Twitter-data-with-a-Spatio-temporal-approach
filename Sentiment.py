af7c4d514ad86163a682d93d8b373315381bfcba
QiqiSu <63663984+qisuqi@users.noreply.github.com>
2020-04-29 20:48:25 +0100

    Update Sentiment.py

--- Sentiment.py
+++ Sentiment.py
@@ -2,7 +2,7 @@ import pandas as pd
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 
-file = pd.read_csv('CleanLocation_after1.csv', engine='python')
+file = pd.read_csv('xxx.csv')
 
 filtered_tweets = str(file['Clean_tweets'])
 
@@ -14,12 +14,6 @@ file = file.drop(columns=['Unnamed: 0','Unnamed: 0.1'])
 
 #for col in file.columns:
     #print(col)
-
-#file.to_csv('CleanLocationSentiment_after.csv')
-
-file1 = pd.read_csv('CleanLocationSentiment.csv',engine='python')
-
-file2 = pd.concat([file,file1],sort=False).reset_index()
 
 plt.subplot(1,2,1)
 plt.hist(file2['Polarity'],color='mediumpurple')
@@ -35,12 +29,3 @@ plt.xlabel('Subjectivity')
 plt.ylabel('Number of tweets')
 plt.tight_layout()
 plt.show()
-
-file2 = file2.drop(columns=['Unnamed: 0','index'])
-
-#file2.to_csv('Final.csv')
-
-#print(len(file2))
-
-#for col2 in file2.columns:
-    #print(col2)
